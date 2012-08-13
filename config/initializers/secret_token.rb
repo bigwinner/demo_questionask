629927205178cc5270b155a2c6cb53d469392c7a
Roger Ahn <roger@stylesparrow.com>
2012-08-14 00:30:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoQuestionask::Application.config.secret_token = '4b216c3c6f13baba0f366f9101238dde12ff377d5fb8fb781887fb1022a832aea693a97e2274de61a02d89f6efdf1d70c9906185bb372a9450b37f5023615b61'
